7c1e3fbf84120afc329dea6be82a90ee13631bc9
lsminter <lsminteris@gmail.com>
2017-09-27 11:44:07 -0700

    fixed extra )

--- rxjs-reactive-programming-why-choose-rxjs/script.js
+++ rxjs-reactive-programming-why-choose-rxjs/script.js
@@ -3,7 +3,7 @@ console.clear();
 var streamA = Rx.Observable.of(3, 4);
 var streamB = streamA.map(a => 10 * a);
 
-streamB.subscribe(b => console.log(b) || displayInPreview(b));
+streamB.subscribe(b => console.log(b) || displayInPreview(b);
 
 
 
